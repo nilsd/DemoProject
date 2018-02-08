e914875462af1dc3e80592acf2ac894578619ecc
nilsd <nils@dunso.se>
2018-02-08 13:23:21 +0100

    Collection view layout

--- DemoProject/features/Photo Browser/controllers/PhotoBrowserViewController.swift
+++ DemoProject/features/Photo Browser/controllers/PhotoBrowserViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class PhotoBrowserViewController: UIViewController {
 
+    // MARK: - Outlets
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    // MARK: - Properties
     
     var dataSource: PhotosBrowserDataSource!
     
+    
+    // MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +32,56 @@ class PhotoBrowserViewController: UIViewController {
         super.viewDidAppear(animated)
         
         dataSource.fetchPhotos()
+    }
+    
+}
+
+
+// MARK: - Collection view layout
+
+extension PhotoBrowserViewController : UICollectionViewDelegateFlowLayout {
+    
+    // MARK: UI properties
+    
+    private var defaultSpacing: CGFloat {
+        return 10
+    }
+    
+    private var sectionInsets: UIEdgeInsets {
+        return UIEdgeInsets(
+            top: defaultSpacing,
+            left: defaultSpacing,
+            bottom: defaultSpacing,
+            right: defaultSpacing
+        )
+    }
+    
+    private var itemsPerRow: CGFloat {
+        switch UIDevice.current.orientation {
+        case .portrait, .portraitUpsideDown:
+            return 2
+        default:
+            return 3
+        }
+    }
+    
+    
+    // MARK: Delegate functions
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
     }
     
 }
