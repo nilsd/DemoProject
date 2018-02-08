6954022b51492525070eb65303d4af0dfbf998ac
nilsd <nils@dunso.se>
2018-02-08 11:59:11 +0100

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Nils Dunsö on 2018-02-08.
+//  Copyright © 2018 Dunso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
